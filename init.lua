0a9c5205e512282a756f058b1449e72ea32f8326
yongi-k <yongi-kim@c-fo.com>
2023-11-07 23:39:52 +0900

    Update settings

--- init.lua
+++ init.lua
@@ -43,7 +43,7 @@ return {
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
         -- "lua_ls",
       },
-      timeout_ms = 1000, -- default format timeout
+      timeout_ms = 3000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end
@@ -51,6 +51,16 @@ return {
     -- enable servers that you already have installed without mason
     servers = {
       -- "pyright"
+      "stree",
+    },
+    config = {
+      stree = function()
+        return {
+          cmd = { "stree", "lsp" },
+          filetypes = { "ruby" },
+          root_dir = require("lspconfig.util").root_pattern(".streerc", "Gemfile", ".git"),
+        }
+      end,
     },
   },
 
@@ -81,5 +91,44 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
+
+    -- textDocument/diagnostic support until 0.10.0 is released
+    _timers = {}
+    local function setup_diagnostics(client, buffer)
+      if require("vim.lsp.diagnostic")._enable then return end
+
+      local diagnostic_handler = function()
+        local params = vim.lsp.util.make_text_document_params(buffer)
+        client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
+          if err then
+            local err_msg = string.format("diagnostics error - %s", vim.inspect(err))
+            vim.lsp.log.error(err_msg)
+          end
+          local diagnostic_items = {}
+          if result then diagnostic_items = result.items end
+          vim.lsp.diagnostic.on_publish_diagnostics(
+            nil,
+            vim.tbl_extend("keep", params, { diagnostics = diagnostic_items }),
+            { client_id = client.id }
+          )
+        end)
+      end
+
+      diagnostic_handler() -- to request diagnostics on buffer when first attaching
+
+      vim.api.nvim_buf_attach(buffer, false, {
+        on_lines = function()
+          if _timers[buffer] then vim.fn.timer_stop(_timers[buffer]) end
+          _timers[buffer] = vim.fn.timer_start(200, diagnostic_handler)
+        end,
+        on_detach = function()
+          if _timers[buffer] then vim.fn.timer_stop(_timers[buffer]) end
+        end,
+      })
+    end
+
+    require("lspconfig").ruby_ls.setup {
+      on_attach = function(client, buffer) setup_diagnostics(client, buffer) end,
+    }
   end,
 }
